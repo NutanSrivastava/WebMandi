0929d02beca446e15a0dadb7b130d3e9fe64bb9c
NutanSrivastava <nuts2197@gmail.com>
2021-02-21 16:20:31 +0530

    uploaded

---
+++ js/custom.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+   // breakpoint and up  
+   $(window).resize(function(){
+       if ($(window).width() >= 980){	
+   
+         // when you hover a toggle show its dropdown menu
+         $(".navbar .dropdown-toggle").hover(function () {
+            $(this).parent().toggleClass("show");
+            $(this).parent().find(".dropdown-menu").toggleClass("show"); 
+          });
+   
+           // hide the menu when the mouse leaves the dropdown
+         $( ".navbar .dropdown-menu" ).mouseleave(function() {
+           $(this).removeClass("show");  
+         });
+     
+           // do something here
+       }	
+   });  
+   
+   // document ready  
+   });
+
+
+
+   $('.banner-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("July 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("deal-timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("deal-timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+$('.product-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+$('.top-product-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+})
